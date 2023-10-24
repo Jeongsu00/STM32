e665e163277cf2289aad856a16c5f42189c66de2
jungsu00 <hakimz@naver.com>
2023-10-24 18:13:54 +0900

    Bitwise-Operator(GPIO)

---
+++ R401RE_Bitwise-Operator/Core/Inc/Led.h
@@ -0,0 +1,16 @@
+/*
+ * Led.h
+ *
+ *  Created on: Oct 24, 2023
+ *      Author: user
+ */
+
+#ifndef INC_LED_H_
+#define INC_LED_H_
+
+#include "main.h"
+
+void Led_Set();
+void Led_Control();
+
+#endif /* INC_LED_H_ */
